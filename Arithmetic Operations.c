d900e5ba48bfa2eeddd092e03fca3b7c8d29e53f
harikrishnanav <harikrishnanav5621@gmail.com>
2026-04-05 14:04:18 +0530

    Add files via upload

---
+++ Arithmetic Operations.c
@@ -0,0 +1,16 @@
+// Harikrishnan A V
+//International school of photonics 
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+
+    printf("Sum = %d\nDifference = %d\nProduct = %d\nQuotient = %.2f",
+    a+b,a-b,a*b,(float)a/b);
+
+    return 0;
+}
